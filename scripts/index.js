4405b039164e086b4a3e0e625bc5416d1b235ae0
Roberto Nicoletti <roberto.urias@gmail.com>
2024-10-24 16:29:27 -0300

    criando js com constante dos elementos

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// Seleciona elementos do HTML
+const fieldNumbers = document.getElementsByClassName('field-numbers');
+const inputNumbers = document.getElementById('numbers');
+const inputOf = document.getElementById('of');
+const inputUntil = document.getElementById('until');
+
+const checkboxRepeat = document.getElementById('repeat');
+const sortForm = document.getElementById('sort-form');
+const results = document.querySelector('#sort-form .results');
+const sortAgain = document.getElementsByClassName('btn-again')[0];
+
